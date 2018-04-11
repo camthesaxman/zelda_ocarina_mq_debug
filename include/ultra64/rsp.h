43d85ea8f7d26e660cb917d46ae8387d7425f735
Nicholas Estelami <NEstelami@users.noreply.github.com>
2018-04-11 00:35:37 -0400

    Added rsp.h, cleaned up code

---
+++ include/ultra64/rsp.h
@@ -0,0 +1,12 @@
+#ifndef _ULTRA64_RSP_H_
+#define _ULTRA64_RSP_H_
+
+/* SP Registers */
+
+#define SP_STATUS_REG 0xA4040010
+#define SP_PC_REG 0xA4080000
+
+/* SP Status Flags */
+#define SP_STATUS_HALT 1
+
+#endif
--- src/code_801067A0.c
+++ src/code_801067A0.c
@@ -1,19 +1,13 @@
 #include <ultra64.h>
 #include <global.h>
 
-// Note: This is part of libultra
-// TODO: CLEAN THIS UP!
-
-#define SP_STATUS_REG 0xA4040010
-#define SP_PC_REG 0xA4080000
-
 s32 func_801067A0(u32 a0)
 {
     register u32 spStatus;
 
     spStatus = *(u32*)(SP_STATUS_REG);
 
-    if (!(spStatus & 1))
+    if (!(spStatus & SP_STATUS_HALT))
         return -1;
 
     *(u32*)(SP_PC_REG) = a0;
